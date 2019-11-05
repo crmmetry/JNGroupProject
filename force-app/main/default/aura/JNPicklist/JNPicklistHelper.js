efaf6de6e4ed1234ca269f0d420cbef339b3bd7b
Remario Richards <remario.richards@crmmetry.com>
2019-11-05 17:43:14 -0500

    fixed JNPicklist not defaulting

--- force-app/main/default/aura/JNPicklist/JNPicklistHelper.js
+++ force-app/main/default/aura/JNPicklist/JNPicklistHelper.js
@@ -1,35 +1,28 @@
 ({
-    getPickListValues: function(component) {
-      const action = component.get("c.getPickListValues");
-      const objName = component.get("v.crmmObjectname");
-      const objField = component.get("v.crmmObjectField");
-      const crmmLabel = component.get("v.crmmLabel");
-      const selected = component.get("v.crmmObjectSelected");
-     
-      action.setParams({
-        'objectApiName': objName,
-        'fieldApiName': objField
-      });
-      action.setCallback(this, function(response) {
-        const state = response.getState();
-        if (state === "SUCCESS") {
-            if(crmmLabel!=null){
-                component.find("picklistChoices").set("v.label",crmmLabel);
-             }
-            if(selected!=null){
-                component.find("picklistChoices").set("v.value", selected);
-            }
+  getPickListValues: function(component) {
+    const action = component.get("c.getPickListValues");
+    const objName = component.get("v.crmmObjectname");
+    const objField = component.get("v.crmmObjectField");
+    const crmmLabel = component.get("v.crmmLabel");
+    const selected = component.get("v.crmmObjectSelected");
 
-          const values = response.getReturnValue();
-           
-          component.set("v.values", values);
-
-        } else {
+    action.setParams({
+      objectApiName: objName,
+      fieldApiName: objField
+    });
+    action.setCallback(this, function(response) {
+      const state = response.getState();
+      if (state === "SUCCESS") {
+        if (crmmLabel != null) {
+          component.find("picklistChoices").set("v.label", crmmLabel);
         }
-      });
-      $A.enqueueAction(action);
-    },
-      createLead: function(component) {
-          
+        const values = response.getReturnValue();
+          if(selected) {
+               component.find("picklistChoices").set("v.crmmObjectSelected", selected);
+          }
+        component.set("v.values", values);
       }
-  });
+    });
+    $A.enqueueAction(action);
+  },
+});
