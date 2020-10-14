1198ab7d6a14faa0a3cb2768646fbf68e85e7179
Travis Allen <travisallen@crmmetry.com>
2020-10-13 19:23:40 -0500

    Field Data attributes for Unsecured Loan Line Item

--- force-app/main/default/aura/UnsecuredLoanFeatureLineItemComponent/UnsecuredLoanFeatureLineItemComponentController.js
+++ force-app/main/default/aura/UnsecuredLoanFeatureLineItemComponent/UnsecuredLoanFeatureLineItemComponentController.js
@@ -1,5 +1,36 @@
 ({
-    myAction : function(component, event, helper) {
+    onInterestedInCreditorLifeChange: function(component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    },
 
-    }
+    onCoverageTypeChange: function(component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    },
+
+    onIncludeCoverageChange: function(component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    },
+
+    onWaveProcessingFeeChange: function(component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    },
+
+    onIncludeWaveProcessingFeeChange: function(component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    },
+
+    onRepaymentMethodChange: function(component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    },
+
+    onDeductFirstMonthRepaymentChange: function(component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    },
 })
