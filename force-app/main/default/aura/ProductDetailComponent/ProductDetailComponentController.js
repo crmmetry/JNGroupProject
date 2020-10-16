d798d7a1714e67430d2941b7f71be67fc55597a5
Travis Allen <travisallen@crmmetry.com>
2020-10-15 21:34:06 -0500

    Product Select Controller Implemented

--- force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentController.js
+++ force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentController.js
@@ -1,7 +1,8 @@
 ({
-    onProductChange: function(component, event, helper) {
-        const selected = event.getSource().get("v.value");
-       console.log(selected);
+    doinit: function(component, event, helper) {
+        // let products = ['Auto', 'Unsecured Loan', 'Credit Card', 'Line of Credit'];
+        // component.set('v.products');
+        helper.updateProductSelection(component);
     },
     
     onLoanPurposeChange: function(component, event, helper) {
--- force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentHelper.js
+++ force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentHelper.js
@@ -1,5 +1,24 @@
 ({
-    helperMethod : function() {
+    updateProductSelection : function(component) {
+        let oppId = component.get("v.recordId");
+        console.log("oppId ", oppId)
+        let productList = component.get("v.products");
+        let productSelection = component.find("selectProduct");
+        let action = component.get("c.opportunityProductSelector");
+        action.setParams({
+            oppId: oppId,
+            productSelections: productList
+        });
+        action.setCallback(this, function (response) {
+            let state = response.getState(); //Checking response status
+            let result = response.getReturnValue();
+            console.log("Result ", result);
+            if (state == "SUCCESS") {
+                console.log("success");
+                productSelection.set("v.value", result);
+            }
+        });
 
+        $A.enqueueAction(action);
     }
 })
