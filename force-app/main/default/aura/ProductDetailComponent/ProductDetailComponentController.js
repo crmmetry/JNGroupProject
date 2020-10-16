3c9437658a2575d915eaafe0ac6c2ada35220ec9
Travis Allen <travisallen@crmmetry.com>
2020-10-16 16:01:20 -0500

    Switch between components logic implemented

--- force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentController.js
+++ force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentController.js
@@ -4,7 +4,7 @@
         // component.set('v.products');
         helper.updateProductSelection(component);
     },
-    
+
     onLoanPurposeChange: function(component, event, helper) {
         const selected = event.getSource().get("v.value");
        console.log(selected);
