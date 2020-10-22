ac95db617bd5330b9552103b159a9d0367bee921
Travis Allen <travisallen@crmmetry.com>
2020-10-22 15:11:20 -0500

    Calculation implementatiom

--- force-app/main/default/aura/CreditCalculationsLineItemComponent/CreditCalculationsLineItemComponentController.js
+++ force-app/main/default/aura/CreditCalculationsLineItemComponent/CreditCalculationsLineItemComponentController.js
@@ -1,3 +1,9 @@
 ({
-  onRequestedDetailsChange: function (component, event, helper) {}
+  onRequestedDetailsChange: function (component, event, helper) {
+    console.log("Requested Details: ", component.get("v.RequestedDetails"));
+    helper.calculateMonthlyP_ILoanAmount(
+      component,
+      component.get("v.RequestedDetails")
+    );
+  }
 });
--- force-app/main/default/aura/CreditCalculationsLineItemComponent/CreditCalculationsLineItemComponentHelper.js
+++ force-app/main/default/aura/CreditCalculationsLineItemComponent/CreditCalculationsLineItemComponentHelper.js
@@ -1,5 +1,46 @@
 ({
-    helperMethod : function() {
+  calculateMonthlyP_ILoanAmount: function (component, requestDetailMap) {
+    let rate;
+    let totalMonths;
+    let pmtResult;
+    if (requestedDetailMap != null) {
+      const fields = [
+        { name: "loanAmount", valid: false },
+        { name: "years", valid: false },
+        { name: "months", valid: false },
+        { name: "market", valid: false }
+      ];
+      fields.forEach((field, index) => {
+        console.log("success foreach");
+        if (requestedDetailMap.hasOwnProperty(field.name)) {
+          if (!isEmpty(requestedDetailMap[field])) {
+            fields[index].valid = true;
+          }
+        }
+      });
+      //Calculations
+      //Calculate Percentage
+      if (fields[3].valid) {
+        rate = calculateRatePerPeriod(requestedDetailMap.market);
+      }
+      if (fields[2].valid && fields[1].valid) {
+        totalMonths = calculateMonths(
+          requestDetailMap.years,
+          requestDetailMap.months
+        );
+      }
 
+      if (rate && totalMonths && requestDetailMap.loanAmount) {
+        pmtResult = calculatePMT(
+          rate,
+          totalMonths,
+          -requestDetailMap.loanAmount,
+          0,
+          0
+        );
+        pmtResult = parseFloat(pmtResult).toFixed(2);
+        component.set("v.monthly_PI_LoanAmount", pmtResult);
+      }
     }
-})
+  }
+});
