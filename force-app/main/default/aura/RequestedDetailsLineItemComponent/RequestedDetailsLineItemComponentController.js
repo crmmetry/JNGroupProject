7fbcabeb426fd6f8bf7f5ede7e3643a816f6d6db
Travis Allen <travisallen@crmmetry.com>
2020-11-11 20:00:58 -0500

    Merge changes

--- force-app/main/default/aura/RequestedDetailsLineItemComponent/RequestedDetailsLineItemComponentController.js
+++ force-app/main/default/aura/RequestedDetailsLineItemComponent/RequestedDetailsLineItemComponentController.js
@@ -15,5 +15,20 @@
     );
     component.set("v.ParentContainer", data);
     console.info(JSON.stringify(component.get("v.ChildContainer")));
+    helper.getApplicants(component);
+  },
+  onApplicantsChange: function (component, event, helper) {
+    let applicant = component.get("v.applicants");
+    const applicantsMap = {
+      id: applicant[0].Id,
+      rating: applicant[0].rating,
+      age: applicant[0].age
+    };
+    const data = Object.assign(
+      component.get("v.ParentContainer"),
+      applicantsMap
+    );
+    component.set("v.ParentContainer", data);
+    console.log(JSON.stringify(data));
   }
 });
