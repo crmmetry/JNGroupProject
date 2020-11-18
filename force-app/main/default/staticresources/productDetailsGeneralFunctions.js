3877aa2579b34bb7f357e6e4917be30a43aa9fb6
Remario Richards <remariorichards@crmmetry.com>
2020-11-18 18:37:03 -0500

    added calculateSavings in productDetailsFunctions

--- force-app/main/default/staticresources/productDetailsGeneralFunctions.js
+++ force-app/main/default/staticresources/productDetailsGeneralFunctions.js
@@ -1,3 +1,39 @@
 /**
  * this file consolidates all the reusable functions used in different aura components
  */
+
+/**
+ * calculates savings
+ * @param {Object} data 
+ * @param {Number} totalMonthly_PI_LoanPayment
+ * @return {Object}
+ */
+function calculateSavings(data, totalMonthly_PI_LoanPayment) {
+    if (validNumbersWithObject(['totalMonthly_PI_LoanPayment','months','years'], data)) {
+        let tenure = calculateMonths(data.years, data.months);
+        let monthlySavings = basicMonthlyCompulsorySavingsCalculator(
+            totalMonthly_PI_LoanPayment,
+            data.percentage,
+            data.amount
+        );
+        let monthlySavingsOverRepaymentPeriod = basicTotalMonthlyCompulsorySavingsCalculator(
+            monthlySavings,
+            tenure
+        );
+        return {
+            totalCompulsorySavingsBalance: parseFloat(monthlySavingsOverRepaymentPeriod),
+            monthlyCompulsorySavings: parseFloat(monthlySavings)
+        }
+    } else if (validNumbersWithObject(['amount'], data)) {
+        let totalCompulsorySavings = data.amount * tenure;
+        return {
+            monthlyCompulsorySavings: data.amount,
+            totalCompulsorySavingsBalance: totalCompulsorySavings
+        }
+    } else {
+        return {
+            totalCompulsorySavingsBalance: 0,
+            monthlyCompulsorySavings: 0
+        }
+    }
+}
