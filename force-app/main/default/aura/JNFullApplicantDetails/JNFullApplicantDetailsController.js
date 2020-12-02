9f45f8652626e6c913b0571b46c4f496a08ce829
Remario Richards <remariorichards@crmmetry.com>
2020-12-02 15:39:25 -0500

    merged with feature/JN1-4047

--- force-app/main/default/aura/JNFullApplicantDetails/JNFullApplicantDetailsController.js
+++ force-app/main/default/aura/JNFullApplicantDetails/JNFullApplicantDetailsController.js
@@ -4,22 +4,11 @@
     helper.getApplicant(component);
     component.set("v.validate", function () {
       helper.updateApplicant(component);
-      /*if (helper.validateFields(component)) {
-
-        return { isValid: true };
-      } else {
-        return {
-          isValid: false,
-          errorMessage: "Please complete the form to continue."
-        };
-      }*/
     });
   },
   //JN1-4047  :: Added a method to set changed selected expense detail
   handleExpenseDetailChange: function (component, event, helper) {
-    debugger;
     var expenseDetail = component.find("monthlyExpensesDetails").get("v.value");
-    console.log("expenseDetail", expenseDetail);
     component.set("v.selectedExpenseDetail", expenseDetail);
   }
 });
