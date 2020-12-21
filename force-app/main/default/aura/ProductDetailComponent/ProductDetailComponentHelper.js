30343e47c2ac65e5088746f01905089f2bf31b25
Travis Allen <travisallen@crmmetry.com>
2020-12-21 10:18:37 -0500

    Minor changes

--- force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentHelper.js
+++ force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentHelper.js
@@ -516,7 +516,6 @@
       container.creditRiskRating
     );
     if (!container.cashInvestmentFlag && validNumber(riskFactor)) {
-      console.log("Cash Investment is false");
       let values = [
         {
           key: "approvedStartingLimit",
@@ -525,19 +524,16 @@
       ];
       let data = updateChildContainerWithValue(component, values, false);
       component.set("v.ChildContainer", data);
-      console.log("No collateral ASL", data.approvedStartingLimit);
       return values;
     } else if (container.cashInvestmentFlag) {
-      console.log("Cash Investment is true");
       let values = [
         {
           key: "approvedStartingLimit",
-          value: ASLCalculator(container, jnDefaults, undefined)
+          value: ASLCalculator(container, jnDefaults, null)
         }
       ];
       let data = updateChildContainerWithValue(component, values, false);
       component.set("v.ChildContainer", data);
-      console.log("Cash collateral ASL", data.approvedStartingLimit);
       return values;
     }
     let values = [{ key: "approvedStartingLimit", value: 0 }];
