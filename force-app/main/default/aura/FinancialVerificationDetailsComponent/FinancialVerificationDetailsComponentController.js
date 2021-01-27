ec4d8ec703c80004e75f1cda94755c25cf26aace
Travis Allen <travisallen@crmmetry.com>
2021-01-27 18:29:11 -0500

    Verification Logic Implemented

--- force-app/main/default/aura/FinancialVerificationDetailsComponent/FinancialVerificationDetailsComponentController.js
+++ force-app/main/default/aura/FinancialVerificationDetailsComponent/FinancialVerificationDetailsComponentController.js
@@ -1,3 +1,24 @@
 ({
-  myAction: function (component, event, helper) {}
+  toggleCheckBoxes: function (component, event, helper) {
+    let checkBoxVar = component.get("v.toggleCheckAll");
+    let buttonCmp = event.getSource();
+    if (checkBoxVar) {
+      component.set("v.toggleCheckAll", false);
+      console.log("check all toggled to false");
+    } else if (!checkBoxVar) {
+      component.set("v.toggleCheckAll", true);
+      console.log("check all toggled to true");
+    }
+    if (buttonCmp.get("v.label") === "Check All") {
+      buttonCmp.set("v.label", "Uncheck All");
+      console.log("button label change");
+    } else {
+      buttonCmp.set("v.label", "Check All");
+      console.log("button label change");
+    }
+  },
+
+  scriptsLoaded: function (component, event, helper) {
+    component.set("v.scriptsLoaded", true);
+  }
 });
--- force-app/main/default/aura/NetWorthVerificationLineComponent/NetWorthVerificationLineComponentController.js
+++ force-app/main/default/aura/NetWorthVerificationLineComponent/NetWorthVerificationLineComponentController.js
@@ -1,3 +1,55 @@
 ({
-  myAction: function (component, event, helper) {}
+  doinit: function (component, event) {
+    let auraList = ["totalAssets", "totalLiabilities", "netWorth"];
+    component.set("v.auraIdList", auraList);
+    console.log("component was initialised");
+  },
+  onToggleCheckAlChange: function (component, event, helper) {
+    console.log("check all change handler");
+    let checkBoxCmp = component.find("verificationToggle");
+    checkBoxCmp.forEach((element) => {
+      element.set("v.checked", component.get("v.toggleCheckAll"));
+      if (component.get("v.toggleCheckAll")) {
+        //set all verified fields to disabled
+        let inputCmpIdList = component.get("v.auraIdList");
+        inputCmpIdList.forEach((element) => {
+          let inputCmp = component.find(element);
+          inputCmp.set("v.disabled", true);
+        });
+      } else {
+        //set all verified fields to undisabled
+        let inputCmpIdList = component.get("v.auraIdList");
+        inputCmpIdList.forEach((element) => {
+          let inputCmp = component.find(element);
+          inputCmp.set("v.disabled", false);
+        });
+      }
+      console.log("component checkbox set to true");
+    });
+  },
+
+  fireComponentEvent: function (cmp, event) {
+    let cmpEvent = cmp.getEvent("NetWorthEvent");
+    let checkBoxCmpName = event.getSource().get("v.name");
+    let checkBoxValue = event.getSource().get("v.checked");
+    console.log(checkBoxCmpName);
+    console.log(checkBoxValue);
+    cmpEvent.setParams({
+      componentName: checkBoxCmpName,
+      checkedVar: checkBoxValue
+    });
+    cmpEvent.fire();
+  },
+
+  handleComponentEvent: function (component, event) {
+    let componentName = event.getParam("componentName");
+    let checkedValue = event.getParam("checkedVar");
+    let inputCmp = component.find(componentName);
+    if (checkedValue) {
+      inputCmp.set("v.disabled", true);
+      //set value to the value of related unverified amount
+    } else {
+      inputCmp.set("v.disabled", false);
+    }
+  }
 });
