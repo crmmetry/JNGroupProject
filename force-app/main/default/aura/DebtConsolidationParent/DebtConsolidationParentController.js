a85d23074a57ff8495557d704df927afb7d46193
ishwari-gaikwad2 <ishwarigaikwad28@gmail.com>
2021-02-03 22:52:40 +0530

    RUB-113 And RUB-117 Changes

---
+++ force-app/main/default/aura/DebtConsolidationParent/DebtConsolidationParentController.js
@@ -0,0 +1,8 @@
+({
+    remove : function(component, event, helper) {
+        var indexPosition = event.target.name;
+        var setEvent = component.getEvent("setAttribute");
+        setEvent.setParams({"rowToDelete":indexPosition});
+        setEvent.fire();
+    },
+})
