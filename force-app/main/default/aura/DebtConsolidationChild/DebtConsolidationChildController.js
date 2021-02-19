dc86bed16ed84564c1e96497c275a29a046458fe
Tushar Chopade <tushar.chopade@thinqloud.com>
2021-02-19 21:32:23 +0530

    Required Files for Ticket RUB

--- force-app/main/default/aura/DebtConsolidationChild/DebtConsolidationChildController.js
+++ force-app/main/default/aura/DebtConsolidationChild/DebtConsolidationChildController.js
@@ -1,8 +1,18 @@
 ({
-  onChange: function (component, event, helper) {
-    let inputfieldValue = component.find("DebtAmount").get("v.value");
-    let setEvent = component.getEvent("setAttribute");
-    setEvent.setParams({ attributeValue: inputfieldValue });
-    setEvent.fire();
-  }
+    Init: function(component, event, helper){
+        helper.getPickListValues(component);
+    },
+    onChange: function (component, event, helper) {
+        let indexPosition = component.find("DebtAmount").get("v.name");
+        let inputfieldValue = component.find("DebtAmount").get("v.value");
+        let setEvent = component.getEvent("setAttribute");
+        setEvent.setParams({
+            attributeValue: indexPosition + ":" + inputfieldValue
+        });
+        setEvent.fire();
+    },
+    onDebtInstitutionChange: function (component, event, helper) {
+        const selected = event.getSource().get("v.value");
+        console.log(selected);
+    }
 });
---
+++ force-app/main/default/aura/DebtConsolidationChild/DebtConsolidationChildHelper.js
@@ -0,0 +1,19 @@
+({
+	getPickListValues: function (component) {
+    const action = component.get("c.getPickListValuesList");
+    action.setParams({
+      objectApiName: "Application_Asset_Liability__c",
+      fieldApiNames: [
+        "Institution_Debt_List__c"
+      ]
+    });
+    action.setCallback(this, function (response) {
+      const state = response.getState();
+      if (state === "SUCCESS") {
+        const mappedList = response.getReturnValue();
+        component.set("v.debtInstitutions", mappedList["Institution_Debt_List__c"]);
+      }
+    });
+    $A.enqueueAction(action);
+  },
+})
