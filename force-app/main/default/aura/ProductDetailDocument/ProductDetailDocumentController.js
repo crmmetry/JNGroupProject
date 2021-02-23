d3aad4d61e0530581dd2b4a7fb07622cb7c3b0b8
Remario Richards <remariorichards@crmmetry.com>
2021-02-22 22:15:30 -0500

    added gotoURL

--- force-app/main/default/aura/ProductDetailDocument/ProductDetailDocumentController.js
+++ force-app/main/default/aura/ProductDetailDocument/ProductDetailDocumentController.js
@@ -1,3 +1,11 @@
 ({
-  onDocumentClick: function (component, event, helper) {}
+  onDocumentClick: function (component, event, helper) {},
+  gotoURL: function (component, event, helper) {
+    let url;
+    let urlEvent = $A.get("e.force:navigateToURL");
+    urlEvent.setParams({
+      url: url
+    });
+    urlEvent.fire();
+  }
 });
