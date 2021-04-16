4101a0e0417dba36839bf7b97a7173f7bd6b217f
Travis Allen <travisallen@crmmetry.com>
2021-04-16 12:47:45 -0500

    Console logs removed

--- force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentHelper.js
+++ force-app/main/default/aura/ProductDetailComponent/ProductDetailComponentHelper.js
@@ -173,10 +173,6 @@
       if (state === "SUCCESS") {
         //update spinner status
         //this.checkSpinnerStatus(component, "assetsAndLiabilitiesForApplicants");
-        console.log(
-          "Applicant Assets and Liabilities",
-          JSON.parse(JSON.stringify(result))
-        );
         this.mergeWithChildContainer(component, result);
         this.existingDebtCalculator(component, result);
       }
